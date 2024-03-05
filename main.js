c285c9bc757f66d5f63b31dbb13aac5656b2f2d6
Guilherme Farias <gfarias02@hotmail.com>
2024-03-05 19:16:52 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,37 @@
+const form = document.getElementById('form-agenda');
+nome = [];
+numero = [];
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+});
+
+function adicionaLinha () {
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputNumeroContato = document.getElementById('numero-contato');
+
+    if (nome.includes(inputNomeContato.value)) {
+        alert(`O contato: ${inputNomeContato.value} já foi inserido(a).`);
+    } else {
+    nome.push(inputNomeContato.value);
+    numero.push(parseFloat(inputNumeroContato.value));
+
+    let linha = '<tr>';
+    linha += `<td>${inputNomeContato.value}</td>`;
+    linha += `<td>${inputNumeroContato.value}</td>`;
+    linha += '</tr>';
+
+    linhas += linha;
+
+    inputNomeContato.value = '';
+    inputNumeroContato.value = '';
+}}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
